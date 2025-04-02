662f850178126cc903266c6086b7a042190664b2
@borisevich.o <borisevich.olga94@gmail.com>
2025-04-02 22:00:28 +0300

    Исправление 3 задачи

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,7 +1,7 @@
 let calculator = {
-  read() {
-    this.a = +prompt('a:', 0);
-    this.b = +prompt('b:', 0);
+  read(a, b) {
+    this.a = a;
+    this.b = b;
   },
   sum() {
     return this.a + this.b;
